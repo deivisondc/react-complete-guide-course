2d76b4e360cbff4ef63c75b3ff7b5594a7b203f2
Deivison Cardoso <deivisondc@gmail.com>
2023-10-29 18:41:12 -0300

    add new state into redux store

--- Section 19 - Diving into Redux/01-starting-project/src/store/index.js
+++ Section 19 - Diving into Redux/01-starting-project/src/store/index.js
@@ -1,24 +1,34 @@
 import { createStore } from 'redux'
 
 const initialState = {
-  counter: 0
+  counter: 0,
+  showCounter: true,
 }
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'increment': {
       return {
+        ...state,
         counter: state.counter + 1,
       }
     }
     case 'increase': {
       return {
+        ...state,
         counter: state.counter + action.amount,
       }
     }
     case 'decrement': {
       return {
+        ...state,
         counter: state.counter - 1,
+      }
+    }
+    case 'toggle': {
+      return {
+        ...state,
+        showCounter: !state.showCounter
       }
     }
     default: {
