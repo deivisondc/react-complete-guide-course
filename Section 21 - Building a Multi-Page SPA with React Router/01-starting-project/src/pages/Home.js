c816ca25129012be0110be8fcb39dd241cc6c81b
Deivison Cardoso <deivisondc@gmail.com>
2023-11-02 10:14:45 -0300

    add new page components

---
+++ Section 21 - Building a Multi-Page SPA with React Router/01-starting-project/src/pages/Home.js
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1>My homepage</h1>;
+}
+
+export default HomePage;
---
+++ Section 21 - Building a Multi-Page SPA with React Router/01-starting-project/src/pages/Products.js
@@ -0,0 +1,5 @@
+function ProductsPage() {
+  return <h1>Product Page!</h1>;
+}
+
+export default ProductsPage;
