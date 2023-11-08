56f8bd74d8b58bf452d44ad8c8f881c09f35db8e
Deivison Cardoso <deivisondc@gmail.com>
2023-11-08 11:32:03 -0300

    section 8 - add two-way-binding to show the case use for refs

--- Section 8 - Working with Refs & Portals/01-starting-project/src/components/Player.jsx
+++ Section 8 - Working with Refs & Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
+
 export default function Player() {
+  const [enteredPlayer, setEnteredPlayer] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleChange(e) {
+    setIsSubmitted(false)
+    setEnteredPlayer(e.target.value)
+  }
+
+  function handleClick() {
+    setIsSubmitted(true)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isSubmitted ? enteredPlayer : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={enteredPlayer} onChange={handleChange} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
