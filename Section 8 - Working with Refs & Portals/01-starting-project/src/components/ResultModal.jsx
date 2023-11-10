2ebc26ea29b81a1195c82b39a1bbecd9edca891b
Deivison Cardoso <deivisondc@gmail.com>
2023-11-10 09:33:25 -0300

    section 8 - fixing game logic

--- Section 8 - Working with Refs & Portals/01-starting-project/src/components/ResultModal.jsx
+++ Section 8 - Working with Refs & Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,7 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
 
-function ResultModal({ result, targetTime }, ref) {
+function ResultModal({ timeRemaining, targetTime, onReset }, ref) {
   const dialog = useRef()
+
+  const userLost = timeRemaining <= 0
+  const formattedTimeRemaining = (timeRemaining / 1000).toFixed(2)
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100)
 
   useImperativeHandle(ref, () => {
     return {
@@ -13,11 +17,13 @@ function ResultModal({ result, targetTime }, ref) {
   
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result} </h2>
-      <p>The target time was <strong>{targetTime} seconds.</strong></p>
-      <p>You stopped the timer with <strong>X seconds left.</strong></p>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your score: {score} </h2>}
 
-      <form method="dialog">
+      <p>The target time was <strong>{targetTime} seconds.</strong></p>
+      <p>You stopped the timer with <strong>{formattedTimeRemaining} seconds left.</strong></p>
+
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
