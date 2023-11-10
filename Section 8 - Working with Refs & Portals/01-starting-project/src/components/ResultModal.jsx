53438316a36460bdbe16407d7f5b05cf4cedc414
Deivison Cardoso <deivisondc@gmail.com>
2023-11-10 18:33:43 -0300

    section 8 - using portal

--- Section 8 - Working with Refs & Portals/01-starting-project/src/components/ResultModal.jsx
+++ Section 8 - Working with Refs & Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from 'react-dom'
 
 function ResultModal({ timeRemaining, targetTime, onReset }, ref) {
   const dialog = useRef()
@@ -15,7 +16,7 @@ function ResultModal({ timeRemaining, targetTime, onReset }, ref) {
     }
   })
   
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
       {!userLost && <h2>Your score: {score} </h2>}
@@ -26,7 +27,8 @@ function ResultModal({ timeRemaining, targetTime, onReset }, ref) {
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   )
 }
 
