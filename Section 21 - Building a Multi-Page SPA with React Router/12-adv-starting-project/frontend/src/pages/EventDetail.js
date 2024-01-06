de2d3fd33bb31cf4994a46c0d9560258e7e57df2
Deivison Cardoso <deivisondc@gmail.com>
2024-01-06 17:35:17 -0300

    section 21 - using useRouteLoaderData to access parent (or higher) loader data

--- Section 21 - Building a Multi-Page SPA with React Router/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ Section 21 - Building a Multi-Page SPA with React Router/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,9 +1,22 @@
-import { useParams } from "react-router-dom"
+import { json, useRouteLoaderData } from "react-router-dom"
+import EventItem from "../components/EventItem"
 
 const EventDetailPage = () => {
-  const params = useParams()
+  const data = useRouteLoaderData('event-detail')
 
-  return <h1>Event Detail - {params.id} </h1>
+  return (
+    <EventItem event={data.event} />
+  )
 }
 
 export default EventDetailPage
+
+export async function loader({ request, params }) {
+  const response = await fetch(`http://localhost:8080/events/${params.eventId}`)
+
+  if (!response.ok) {
+    throw json({ message: 'Could not fetch details for selected event.'}, { status: 500 })
+  } else {
+    return response
+  }
+}
