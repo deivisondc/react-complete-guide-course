1f178da5c2bda7fd5d14909e2e1e806a1eea8fa6
Deivison Cardoso <deivisondc@gmail.com>
2024-01-06 17:05:34 -0300

    section 21 - handling multi-errors throwing Responses on loader

--- Section 21 - Building a Multi-Page SPA with React Router/12-adv-starting-project/frontend/src/pages/Error.js
+++ Section 21 - Building a Multi-Page SPA with React Router/12-adv-starting-project/frontend/src/pages/Error.js
@@ -1,5 +1,34 @@
+import { useRouteError } from 'react-router-dom'
+
+import PageContent from "../components/PageContent"
+import MainNavigation from '../components/MainNavigation'
+
 const ErrorPage = () => {
-  return <h1>An error occurred</h1>
+  // if a response is thrown then this object will contain the status property
+  // otherwise it will be the plain object thrown
+  const error = useRouteError()
+
+  let title = 'An error occurred!'
+  let message = 'Something went wrong!'
+
+  if (error.status === 500) {
+    // since a response was thrown, we have to use the data property to access the content
+    message = JSON.parse(error.data).message
+  }
+
+  if (error.status === 404) {
+    title = 'Not found!'
+    message = 'Could not find resource or page.'
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  )
 }
 
 export default ErrorPage
