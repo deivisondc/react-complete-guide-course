5142af6cd2291c1ac3a8415d11cb7a9bef62a3f1
Deivison Cardoso <deivisondc@gmail.com>
2024-01-06 17:48:18 -0300

    section 21 - adding form action through router definition

--- Section 21 - Building a Multi-Page SPA with React Router/12-adv-starting-project/frontend/src/App.js
+++ Section 21 - Building a Multi-Page SPA with React Router/12-adv-starting-project/frontend/src/App.js
@@ -3,7 +3,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import HomePage from './pages/Home'
 import EventsPage, { loader as eventsLoader } from './pages/Events'
 import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetail'
-import NewEventPage from './pages/NewEvent'
+import NewEventPage, { action as newEventAction } from './pages/NewEvent'
 import EditEventPage from './pages/EditEvent'
 import RootLayout from './pages/Root'
 import EventsRootLayout from './pages/EventsRoot'
@@ -50,7 +50,11 @@ const router = createBrowserRouter([
           ]
         },
         // { path: ':id', element: <EventDetailPage />, loader: eventDetailLoader },
-        { path: 'new', element: <NewEventPage /> },
+        { 
+          path: 'new', 
+          element: <NewEventPage />,
+          action: newEventAction
+        },
       ] },
     ]
     // create notes first and then finish
