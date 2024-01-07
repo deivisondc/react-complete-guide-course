cefb8c4d9785bce8e101306af3dba5434efe9af7
Deivison Cardoso <deivisondc@gmail.com>
2024-01-06 22:00:55 -0300

    section 21 - using useFetcher to trigger action from a specific route without navigating to it

--- Section 21 - Building a Multi-Page SPA with React Router/12-adv-starting-project/frontend/src/components/NewsletterSignup.js
+++ Section 21 - Building a Multi-Page SPA with React Router/12-adv-starting-project/frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,33 @@
+import { useFetcher } from 'react-router-dom';
 import classes from './NewsletterSignup.module.css';
+import { useEffect } from 'react';
 
 function NewsletterSignup() {
+  // useFetcher when you want to trigger an action or loader
+  // without actually load the page that the action / load belongs
+  const fetcher = useFetcher()
+  const { data, state } = fetcher;
+
+  useEffect(() => {
+    if (state === 'idle' && data && data.message) {
+      window.alert(data.message)
+    }
+  }, [data, state])
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form
+      method="post"
+      // must have the action pointing to the route that has the action / loader
+      action="/newsletter"
+      className={classes.newsletter}
+    >
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
